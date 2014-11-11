2cde777ddab66389cd3966c65089a77411f41c69
fangjian <fangjianmail@126.com>
2014-11-11 14:16:25 +0800

    min depth ok

--- scala/src/main/scala/com/fangjian/leetcode/MinimumDepthOfBinaryTree.scala
+++ scala/src/main/scala/com/fangjian/leetcode/MinimumDepthOfBinaryTree.scala
@@ -28,7 +28,21 @@ class MinimumDepthOfBinaryTree {
 }
 
 class TreeNode2( value:Int, left:TreeNode2, right:TreeNode2) extends TreeNode(value,left,right){
+     def minDepth:Int={
+        if (left==null && right==null){
+            1
+        }else if(left==null && right!=null){
+            right.minDepth+1
+        }else if(right==null && left!=null){
+            left.minDepth+1
+        }else{
+            Math.min(left.minDepth,right.minDepth)+1;
+        }
+     }
 
+    def this(value:Int)={
+        this(value,null,null)
+    }
 }
 
 //TEST
@@ -38,6 +52,13 @@ class MinimumDepthOfBinaryTreeTest{
 
     @Test
     def test()={
-
+        val root=new TreeNode2(5,
+            new TreeNode2(4 ,new TreeNode2(11, new TreeNode2(7),new TreeNode2(2)),null),
+            new TreeNode2(8,new TreeNode2(13),new TreeNode2(4,null,new TreeNode2(1))))
+        //println(root)
+        root.prettyPrint()
+        println();
+        val result=root.minDepth;
+        Assert.assertEquals(3,result)
     }
 }
